82468c46799f1796bdf6ebfc1f5c3c9e9874a14d
piusidogun <110743770+piusidogun@users.noreply.github.com>
2022-09-08 19:30:45 +0100

    Add files via upload

--- learn.dart
+++ learn.dart
@@ -1,108 +1,58 @@
-// void main() {
-//   Greeting unit1 = Greeting('hello');
-//   print(unit1.sayhelo);
-//   print(unit1.addnum(2, 3));
-//   print(unit1.mutiply(2, 4));
-// }
-
-// class Greeting {
-//   Greeting(this.sayhelo);
-//   String sayhelo;
-//   void greetings() => print(sayhelo);
-//   int addnum(int num1, int num2) => num1 + num2;
-//   int mutiply(int num1, int num2) => num1 + num2;
-// }
-
-// void main() {
-//   Jss1 A = Jss1(
-//       firstname: 'pius',
-//       lastname: 'idogun',
-//       subject: 'chemistry',
-//       score: 90,
-//       age: 17);
-//   print('pius');
-//   print(A.remark());
-//   print(A.isfirstname('pius'));
-//   print(A.mycomment());
-//   print(A.jss2(70));
-// }
-
-// class Jss1 {
-//   Jss1(
-//       {required this.firstname,
-//       required this.lastname,
-//       required this.subject,
-//       required this.score,
-//       required this.age});
-
-//   String firstname;
-//   String lastname;
-//   String subject;
-//   int score;
-//   int age;
-
-//   String remark() =>
-//       'i am $firstname $lastname, am in class three. my best subject is $subject';
-
-//   bool isfirstname(String firstname) {
-//     if (this.firstname == 'efe') {
-//       return true;
-//     } else {
-//       return false;
-//     }
-//   }
-
-//   String mycomment() => '$firstname is promoted to Jss2';
-
-//   String jss2(int score) {
-//     if (this.score < 90) {
-//       return 'promoted to Jss2';
-//     } else {
-//       return 'repeat jss1';
-//     }
-//   }
-// }
-
-// void main() {
-//   var person = Person(age: 20, name: 'tobi');
-//   print(person);
-//   print(person.age);
-//   print(person.personage());
-// }
-
-// class Person {
-//   String name;
-//   int age;
-
-//   Person({required this.name, required this.age});
-
-//   String personage() {
-//     return '$name will be 100years in time';
-//   }
-// }
-// // }
-
 import 'dart:io';
 
 void main() {
-  // print("object");
+  Jss1 A = Jss1(
+      firstname: 'pius',
+      lastname: 'idogun',
+      subject: 'chemistry',
+      score: 90,
+      age: 17);
+  print('pius');
+  print(A.remark());
+  print(A.isfirstname('pius'));
+  print(A.mycomment());
+  print(A.jss2(70));
   stdout.write("What's your name? ");
-  // String? name = stdin.readLineSync();
-  // print("Hi, $name! What is your age?");
-  // int age = int.parse("45");
+  String? name = stdin.readLineSync();
+  print("Hi, $name! What is your age?");
+  int age = int.parse("45");
 
-  // int yearsToHunderd = 100 - age;
-  // print("$name, You have $yearsToHunderd years to be 100");
+  int yearsToHunderd = 100 - age;
+  print("$name, You have $yearsToHunderd years to be 100");
 }
 
+class Jss1 {
+  Jss1(
+      {required this.firstname,
+      required this.lastname,
+      required this.subject,
+      required this.score,
+      required this.age});
 
- 
-// void main() {
-//   stdout.write("Please choose a number: ");
-//   int number = int.parse(stdin.readLineSync());
-//   for (var i = 1; i <= number; i++) {
-//     if (number % i == 0) {
-//       print(i);
-//     }
-//   }
-// }
+  String firstname;
+  String lastname;
+  String subject;
+  int score;
+  int age;
+
+  String remark() =>
+      'i am $firstname $lastname, am in class three. my best subject is $subject';
+
+  bool isfirstname(String firstname) {
+    if (this.firstname == 'efe') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  String mycomment() => '$firstname is promoted to Jss2';
+
+  String jss2(int score) {
+    if (this.score < 90) {
+      return 'promoted to Jss2';
+    } else {
+      return 'repeat jss1';
+    }
+  }
+}
